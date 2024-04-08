44e60dc1c26aedfc6a6e497008a1c9810172f6a2
Kojo Lemaire <bigjolemaire@gmail.com>
2024-04-08 22:30:58 +0000

    Item: created itemInfo()

--- Item.java
+++ Item.java
@@ -57,8 +57,14 @@ public class Item {
         }
     }
 
+    public void itemInfo() {
+        System.out.println("Item ID: " + recordID + "\nName: " + name + "\nQuantity available in inventory: " + quantAvailable + "\nPrice: " + price);
+    }
+
     public static void main(String[] args) {
         Item item = new Item("001", "null", 3, 4);
+        System.out.println(item.getInstanceVar("recordID"));
+        item.setInstanceVar("recordID", "111");
         System.out.println(item.getInstanceVar("recordID"));
     }
 }
