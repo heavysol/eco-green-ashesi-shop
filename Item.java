4335a4c636b7e14b2fb6eeaf5d4909ac9d07c2ef
Kojo Lemaire <bigjolemaire@gmail.com>
2024-04-11 12:58:49 +0000

    Item: Changed name of file from Item01 to Item

--- Item.java
+++ Item.java
@@ -67,7 +67,7 @@ public class Item {
     }
 
     /*public static void main(String[] args) {
-        Item item = new Item("001", "null", 3, 4);
+        Item01 item = new Item01("001", "null", 3, 4);
         System.out.println(item.getInstanceVar("recordID"));
         item.setInstanceVar("recordID", "111");
         System.out.println(item.getInstanceVar("recordID"));
