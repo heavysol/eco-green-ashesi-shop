0b1ad83c879577e83b30ed0477253983b6bedf99
Kojo Lemaire <bigjolemaire@gmail.com>
2024-04-12 17:39:42 +0000

    Item: modified item

--- Item.java
+++ Item.java
@@ -2,6 +2,7 @@
 
 import java.lang.Integer;
 import java.lang.Double;
+import java.util.Arrays;
 
 public class Item {
     // Instance vars
@@ -9,12 +10,26 @@ public class Item {
     Integer quantAvailable;
     Double price;
 
+    static final String[] ITEMS = {
+        "herbal tea",
+        "reusable water bottle",
+        "reusable pack", 
+        "kraft paper bag",
+        "bamboo toothbrush",
+        "bamboo straw",
+        "paper envelope", 
+        "re-usable pad",
+        "recycled bathroom tissue",
+        "local honey",
+    };
+
     // Constructor
     public Item(String recordID, String name, int quantAvailable, double price) {
         this.recordID = recordID;
-        this.name = name;
         this.quantAvailable = (Integer) quantAvailable;
         this.price = (Double) price;
+
+        if (Arrays.asList(ITEMS).contains(name.toLowerCase())) this.name = name;
     }
 
     // gets instance var's value specified in varName parameter
@@ -26,7 +41,7 @@ public class Item {
         return name;
     }
 
-    public int getQuantAvailable() {
+    public Integer getQuantAvailable() {
         return quantAvailable;
     }
 
@@ -41,7 +56,13 @@ public class Item {
                 recordID = (String) newVal;
                 break;
             case "name":
-                name = (String) newVal;
+                String n = (String) newVal;
+                n.toLowerCase();
+                if (Arrays.asList(ITEMS).contains(n)) name = n; 
+                else {
+                    System.out.println("Invalid value. Enter items in the list."); 
+                    return;
+                }
                 break;
             case "quantAvailable":
                 quantAvailable = (Integer) newVal;
@@ -59,11 +80,10 @@ public class Item {
         System.out.println("Item ID: " + recordID + "\nName: " + name + "\nQuantity available in inventory: " + quantAvailable + "\nPrice: " + price);
     }
 
-    /*public static void main(String[] args) {
-        Item01 item = new Item01("001", "null", 3, 4);
-        System.out.println(item.getInstanceVar("recordID"));
-        item.setInstanceVar("recordID", "111");
-        System.out.println(item.getInstanceVar("recordID"));
+    public static void main(String[] args) {
+        Item item = new Item("001", "Reusable water bottle", 3, 4);
+        item.setInstanceVar("name", "herbal tea");
+        System.out.println(item.getName());
         item.itemInfo();
-    }*/
+    }
 }
